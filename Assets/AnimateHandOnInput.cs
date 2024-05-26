08d23fe276d8e214c221b56bb3e967bda5794991
김승태 <tiga1207@naver.com>
2024-05-27 02:57:51 +0900

    0527 part3

--- Assets/AnimateHandOnInput.cs
+++ Assets/AnimateHandOnInput.cs
@@ -6,17 +6,21 @@ using UnityEngine.InputSystem;
 public class AnimateHandOnInput : MonoBehaviour
 {
     public InputActionProperty pinchAnimationAction;
-    // Start is called before the first frame update
+    public InputActionProperty gripAnimationAction;
+
+    public Animator handAnimator;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         float triggerValue = pinchAnimationAction.action.ReadValue<float>();
-
-        Debug.Log(triggerValue);
+        handAnimator.SetFloat("Trigger",triggerValue);
+        
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip",gripValue);
     }
 }
